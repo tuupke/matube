a575acbba94602c164e7b5f851641144a2a236c6
Jonathan Sumrall <jmsumrall@gmail.com>
2014-10-26 11:41:05 +0100

    clarifying the main listening processes files

--- serverBackend/completedJobListener.py
+++ serverBackend/completedJobListener.py
@@ -2,5 +2,6 @@ __author__ = 'max'
 """
 Listens for notifications about completed jobs. When notification comes in, the file should have been rsynced to
 this servers file directory. It is this processes responsibility to remove this entry from the PendingJob table in the database,
-add it to the Completed job table, and notify the user to download the file. 
+add it to the Completed job table, and notify the user to download the file.
 """
+
