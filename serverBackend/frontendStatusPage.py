aa22e379efd4e72cbaf1b9a52c3432f1f77fa4f8
Jonathan Sumrall <jmsumrall@gmail.com>
2014-10-27 10:30:47 +0100

    testing concurrent updates

--- serverBackend/frontendStatusPage.py
+++ serverBackend/frontendStatusPage.py
@@ -7,42 +7,25 @@ import json
 import threading
 import web
 
-msg = 'hello world'
 
-def statusUpdater():
-    connection = pika.BlockingConnection(pika.ConnectionParameters(
-            host='localhost'))
-    channel = connection.channel()
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+        host='localhost'))
+channel = connection.channel()
 
-    channel.queue_declare(queue='status', durable=True)
+channel.queue_declare(queue='status', durable=True)
 
 
-    print ' [*] Waiting for messages. To exit press CTRL+C'
+print ' [*] Waiting for messages. To exit press CTRL+C'
 
-    def callback(ch, method, properties, body):
-        print " [x] Received %r" % (body,)
-        super.msg = body
-
-        print " [x] Done"
-        ch.basic_ack(delivery_tag = method.delivery_tag)
-
-    channel.basic_qos(prefetch_count=1)
-    channel.basic_consume(callback,
-                          queue='status')
-
-    channel.start_consuming()
+def callback(ch, method, properties, body):
+    print " [x] Received %r" % (body,)
 
 
-t_msg = threading.Thread(target=statusUpdater)
-t_msg.start()
+    print " [x] Done"
+    ch.basic_ack(delivery_tag = method.delivery_tag)
 
-urls = (
-    '/', 'index')
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(callback,
+                      queue='status')
 
-class index:
-    def GET(self):
-        return msg
-
-if __name__ == "__main__":
-    app = web.application(urls, globals())
-    app.run()
+channel.start_consuming()
