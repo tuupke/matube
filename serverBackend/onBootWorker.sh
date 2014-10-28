ddaaff7fb8e6b264ad43b0ef7de25c6d4da841a2
root <jmsumrall@gmail.com>
2014-10-28 08:48:02 -0400

    added startup script

---
+++ serverBackend/onBootWorker.sh
@@ -0,0 +1,3 @@
+screen -d -m python /root/matube/serverBackend/health-server.py
+screen -d -m python /root/matube/serverBackend/workerServerListener.py
+echo " Started health-server and server listener"
