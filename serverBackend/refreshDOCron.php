842211422e89f23ad3a437b66b44f6d0e1a4292d
root <root@matube.gehack.nl>
2014-11-04 13:34:07 -0500

    Fixed the cron thing (Mart)

--- serverBackend/refreshDOCron.php
+++ serverBackend/refreshDOCron.php
@@ -1,11 +1,16 @@
 <?php
 
-implode("",exec(`ps aux | grep refreshDO.py`,$lines));
+$lines;
 
+exec('ps aux | grep refreshDO.py', $lines);
+
+$debug = time()." ";
 
 if(strpos("none",file_get_contents("servers.txt")) && strpos("python",$lines)!==false){
+	$debug .= "Refreshing";
 	`python refreshDO.py`;
 }
 
+file_put_contents("debug", $debug."\n", FILE_APPEND);
 
 ?>
