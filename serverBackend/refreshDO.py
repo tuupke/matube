bdcb2f96013189d7c90c881b8fc879eceac9f0b5
Jonathan Sumrall <jmsumrall@gmail.com>
2014-11-04 13:04:42 +0100

    status page

---
+++ serverBackend/refreshDO.py
@@ -0,0 +1,23 @@
+__author__ = 'max'
+import digitalocean
+import json
+token = "f32d2fa8bbc1199eb4c079a1f70de31155778c1f0aef9e17226ce3869dd51a81"
+
+manager = digitalocean.Manager(token=token)
+filename = 'servers.txt'
+fp = open(filename,'w')
+servers = []
+drops = manager.get_all_droplets()
+for drop in drops:
+    if "2IN28" in drop.name:
+        server = {'ip': drop.ip_address,
+                  'name': drop.name,
+                  'memory': drop.memory,
+                  'vcpus': drop.vcpus,
+                  'private_ip': drop.private_ip_address}
+        servers.append(server)
+
+filename = 'servers.txt'
+fp = open(filename,'w')
+json.dump(servers, fp)
+fp.close()
--- serverBackend/status.py
+++ serverBackend/status.py
@@ -4,19 +4,17 @@ import json
 import urllib2
 import sys
 
-token = "f32d2fa8bbc1199eb4c079a1f70de31155778c1f0aef9e17226ce3869dd51a81"
-
-manager = digitalocean.Manager(token=token)
-
 def main():
     if (len(sys.argv) == 1) or ('all' in sys.argv[1]):
-        print get_all()
-    elif 'worker' in sys.argv[1]:
-        print get_workers()
-    elif 'fileserver' in sys.argv[1]:
-        print get_fileserver()
+        servers = get_servers()
+        for server in servers:
+            print getStatus(server)
 
-
+def get_servers():
+    filename = 'servers.txt'
+    fp = open(filename, 'r')
+    servers = json.load(fp)
+    return servers
 
 def getHealth(serverIP):
     response = urllib2.urlopen("http://" + str(serverIP) + ":8080").read()
@@ -24,55 +22,11 @@ def getHealth(serverIP):
 
 def getStatus(server):
     try:
-        status = json.loads(getHealth(str(server.ip_address)))
-        status['name'] = server.name
-        status['memory'] = server.memory
-        status['vcpus'] = server.vcpus
-        status['disk'] = server.disk
-        status['region'] = server.region
-        status['status'] = server.status
-        status['id'] = server.id
-        status['actions'] = []
-        actions = server.get_actions()
-        for action in actions:
-            action.load()
-            status['actions'].append(action.status)
-        return status
+        status = json.loads(getHealth(server['ip']))
+        for key in status.keys():
+            server[key] = status[key]
+
+        return server
     except:
-        return "Server Not responding"
-
-def get_all():
-    servers = []
-    for drop in manager.get_all_droplets():
-        if "2IN28" in drop.name:
-            servers.append(drop)
-
-    responses = []
-    for server in servers:
-        responses.append(getStatus(server))
-    return json.dumps(responses, indent=4, separators=(',', ': '))
-
-def get_workers():
-    servers = []
-    for drop in manager.get_all_droplets():
-        if "worker" in drop.name:
-            servers.append(drop)
-
-    responses = []
-    for server in servers:
-        responses.append(getStatus(server))
-    return json.dumps(responses, indent=4, separators=(',', ': '))
-
-
-def get_fileserver():
-    servers = []
-    for drop in manager.get_all_droplets():
-        if "fileserver" in drop.name:
-            servers.append(drop)
-
-    responses = []
-    for server in servers:
-        responses.append(server)
-    return json.dumps(responses, indent=4, separators=(',', ': '))
-
+        return "{Server not responding}"
 main()
