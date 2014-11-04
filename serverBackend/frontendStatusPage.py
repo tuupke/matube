9cb14a3dfbdee4b7bcb7c672efb680c9c766ca26
Jonathan Sumrall <jmsumrall@gmail.com>
2014-11-04 10:09:05 +0100

    status page

--- serverBackend/frontendStatusPage.py
+++ serverBackend/frontendStatusPage.py
@@ -1,31 +1,90 @@
-__author__ = 'max'
-
-
-import pika
-from utilsForStats import *
-import json
-import threading
+import digitalocean
 import web
+import json
+import urllib2
+
+token = "f32d2fa8bbc1199eb4c079a1f70de31155778c1f0aef9e17226ce3869dd51a81"
+
+manager = digitalocean.Manager(token=token)
+
+urls = (
+    '/', 'index',
+    '/workers', 'workers',
+    '/fileserver', 'fileserver',
+    '/frontend', 'frontend',
+)
+
+def getHealth(self, serverIP):
+    response = urllib2.urlopen("http://" + str(serverIP) + ":8080").read()
+    return response
+
+def getStatus(server):
+    status = json.loads(self.getHealth(str(server.ip_address)))
+    status['name'] = server.name
+    status['memory'] = server.memory
+    status['vcpus'] = server.vcpus
+    status['disk'] = server.disk
+    status['region'] = server.region
+    status['status'] = server.status
+    status['id'] = server.id
+    status['actions'] = []
+    actions = server.get_actions()
+    for action in actions:
+        action.load()
+        status['actions'].append(action.status)
+    return status
+
+class index:
+    def GET(self):
+        servers = []
+        for drop in manager.get_all_droplets():
+            if "2IN28" in drop.name:
+                servers.append(drop)
+
+        responses = []
+        for server in servers:
+            responses.append(getStatus(server))
+        return json.dumps(responses, indent=4, separators=(',', ': '))
+
+class workers:
+    def GET(self):
+        servers = []
+        for drop in manager.get_all_droplets():
+            if "worker" in drop.name:
+                servers.append(drop)
+
+        responses = []
+        for server in servers:
+            responses.append(getStatus(server))
+        return json.dumps(responses, indent=4, separators=(',', ': '))
 
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(
-        host='localhost'))
-channel = connection.channel()
+class fileserver:
+    def GET(self):
+        servers = []
+        for drop in manager.get_all_droplets():
+            if "fileserver" in drop.name:
+                servers.append(drop)
 
-channel.queue_declare(queue='status', durable=True)
+        responses = []
+        for server in servers:
+            responses.append(server)
+        return json.dumps(responses, indent=4, separators=(',', ': '))
 
 
-print ' [*] Waiting for messages. To exit press CTRL+C'
+class frontend:
+    def GET(self):
+        servers = []
+        for drop in manager.get_all_droplets():
+            if "frontend" in drop.name:
+                servers.append(drop)
 
-def callback(ch, method, properties, body):
-    print " [x] Received %r" % (body,)
+        responses = []
+        for server in servers:
+            responses.append(server)
+        return json.dumps(responses, indent=4, separators=(',', ': '))
 
 
-    print " [x] Done"
-    ch.basic_ack(delivery_tag = method.delivery_tag)
-
-channel.basic_qos(prefetch_count=1)
-channel.basic_consume(callback,
-                      queue='status')
-
-channel.start_consuming()
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.run()
