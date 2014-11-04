1bc544cfd297c2061b34432bbb0c611545a193f8
Jonathan Sumrall <jmsumrall@gmail.com>
2014-11-04 11:48:10 +0100

    status page

--- serverBackend/onBootFileServer.sh
+++ serverBackend/onBootFileServer.sh
@@ -1,2 +1,3 @@
+git -C /root/matube/ pull
 screen -d -m python /root/matube/serverBackend/fileserverWebListener.py
 screen -d -m python /root/matube/serverBackend/fileserverCompletedJobListener.py
--- serverBackend/onBootWorker.sh
+++ serverBackend/onBootWorker.sh
@@ -1,3 +1,4 @@
+git -C /root/matube/ pull
 screen -d -m python /root/matube/serverBackend/health-server.py
 screen -d -m python /root/matube/serverBackend/workerServerListener.py
 echo " Started health-server and server listener"
