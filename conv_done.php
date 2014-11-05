aafeec3276f7f486196d2f0a267f52f1282fb38d
Mart Pluijmaekers <mart@gewis.nl>
2014-11-05 15:34:25 +0100

    Fixed conversion update script

--- conv_done.php
+++ conv_done.php
@@ -1,6 +1,7 @@
 <?php
 
 if(!isset($_GET['newName']) || !isset($_GET['oldName']) || !isset($_GET['hash']) || $_GET['hash']!="41dc8c4ced0a3ec02593499f3f58fec306dc58903c054abaff5045ee9f189a96"){
+	echo 'aaa';
 	exit;
 }
 
@@ -11,11 +12,11 @@ require_once("system/database.php");
 
 $db = new Database();
 
-$res = $db->query("select * from video where storage=?", array($id));
+$res = $db->query("select * from video where storage=?", array($old));
 
 $ignore = array("the", "one", "a");
 if(count($res)==1) {
-	$db->nquery("update video set status = 1,storage= ? where storage=?",array($id, $oldName));
+	$db->nquery("update video set status=1, storage=? where storage=?",array($id, $old));
 	$res = $res[0];
 
 	$name = explode(" ", $res[2]);
