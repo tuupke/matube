4e72d40c30d6b6bec01d08842ae48b59d25b580c
Mart Pluijmaekers <mart@gewis.nl>
2014-11-06 12:37:21 +0100

    Fixed video location

--- pages/view.php
+++ pages/view.php
@@ -34,11 +34,25 @@ h3 {
 </style>
 <span style='display: inline-block;'>
 <video id='video' controls>
-	<source src='videos/<?php echo $r[3]; ?>' type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'>
+	<source src='http://178.62.252.40/videos/<?php echo $r[3]; ?>' type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'>
 </video><br />
 <h1 id='title'><?php echo $r[2]; ?></h1><h3 id='views' style='float: right;'>Views <?php echo $r[5]; ?></h3><br />By: <b><?php echo $r[12]; ?></b>
+
 </span>
 <?php
+
+	if($r['public']==0 && $user->getId() == $r['ownedBy']){
+		?><br /><br />
+              <label for="videoName">Invite user to view your video</label>
+                <input id="videoName" name="videoName" type="text" placeholder="Name" style='width: 320px;' class="form-control input-md"><br />
+		<?
+	}
+
+?>
+
+<?php
+
+
 		} else {
 ?>
 <h1>Video is private!</h1>
